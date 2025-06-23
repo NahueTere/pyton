f60b27af367b94fa716c1fbe0e34d8ee9e343e9d
NahueTere <204004234+Bladkars@users.noreply.github.com>
2025-06-23 10:45:36 -0300

    1

---
+++ vectores y matrices/1.py
@@ -0,0 +1,10 @@
+i = 1
+F = []
+print("Ingrese Número de elementos a Ingresar: ")
+n = int( input())
+
+while i <= n:
+    elemento = int( input("Ingrese Elemento: "))
+    F.append(elemento)
+    i = i + 1
+print(F)
